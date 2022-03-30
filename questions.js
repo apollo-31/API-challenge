361271edd9e6a554494abe8fc9433946487efb1f
apollo-31 <46492175+apollo-31@users.noreply.github.com>
2022-03-30 00:01:51 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+//creating an array and passing the number, questions, options and answers
+let questions = [
+    {
+        numb: 1,
+        question: "What is NOT true about JavaScript?",
+        answer: "JavaScript is related to Java and Python.",
+        options: [
+            "JavaScript allows you to manipulate HTML elements.",
+            "JavaScript should not be mistaken for Java,or compared to Python.",
+            "You can modify CSS properties using JavaScript.",
+            "JavaScript is related to Java and Python.",
+        ]
+    },
+    {
+        numb: 2,
+        question: "Inside which HTML element do we put the JavaScript?",
+        answer: "script",
+        options: [
+            "JavaScript",
+            "js",
+            "script",
+            "scripting",
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the correct syntax for referring to an external script called 'xxx.js?'",
+        answer: "script src = 'xxx.js'",
+        options: [
+            "script href = 'xxx.js'",
+            "script src = 'xxx.js'",
+            "script name = 'xxx.js'",
+            "scripting href = 'xxx.js'",
+        ]
+    },
+    {
+        numb: 4,
+        question: "How can you add a single comment in a JavaScript?",
+        answer: "// This is a comment",
+        options: [
+            "// This is a comment",
+            '!--This is a comment--',
+            "'This is a comment'",
+            "/*This is a comment*/",
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which operator is used to assign a value to a variable?",
+        answer: "=",
+        options: [
+            "=",
+            "+",
+            "$",
+            "X",
+        ]
+    },
+];
